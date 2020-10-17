ffaa3a10cef166c2204cabd0e81ace8757f95361
fndsn <fndsn@users.noreply.github.com>
2020-10-17 22:59:57 +0300

    Initial commit

---
+++ Assets/Scripts/Shootables.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shootables : MonoBehaviour {
+	public int maxHealth = 3;
+	public int currentHealth;
+
+	//public HealthBar healthBar;
+
+	private void Start()
+	{
+		currentHealth = maxHealth;
+		//healthBar.SetMaxHealth(maxHealth);
+	}
+
+	public void Damage(int damageAmount)
+	{
+
+		currentHealth -= damageAmount;
+
+		if (currentHealth <= 0)
+		{
+			gameObject.SetActive(false);
+		}
+
+	}
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+        //healthBar.SetHealth(currentHealth);
+	}
+}
+
